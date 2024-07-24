8f188a5f96d275f3b5ed33b3ab7a4782069c5146
Win Ko <wkkn2580@gmail.com>
2024-07-24 08:27:17 +0630

    updated 51

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import PlayGroud from "components/PlayGroud";
+import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
   return (
     <SafeAreaView className="flex-1 p-6">
+      <Link href="/parallax-image">Parallax Image</Link>
       <PlayGroud />
     </SafeAreaView>
   );
